d69d41923575276d3075326bbf593dcfefeaabda
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-04-11 13:43:21 +0200

    tf-agents_train.py

--- tf-agents_train.py
+++ tf-agents_train.py
@@ -30,17 +30,4 @@ parser.add_argument('--env-name', type=str, default='AirSimEnv-v42')
 parser.add_argument('--weights', type=str, default=None)
 args = parser.parse_args()
 
-# Get the environment and extract the number of actions.
-env = gym.make(args.env_name)
-np.random.seed(123)
-env.seed(123)
-nb_actions = env.action_space.n
-
-
-# Next, we build our model. We use the same model that was described by Mnih et al. (2015).
-INPUT_SHAPE = (30, 100)
-WINDOW_LENGTH = 1
-# Next, we build our model. We use the same model that was described by Mnih et al. (2015).
-input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
-
 environment = suite_gym.load(args.env_name)
