71d6fe0ae657b8788340447749ea9f40e6afb04b
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-04-11 16:32:33 +0200

    tf-agents_train.py

--- tf-agents_train.py
+++ tf-agents_train.py
@@ -36,3 +36,10 @@ print('time_step_spec.observation:', environment.time_step_spec().observation)
 print('time_step_spec.step_type:', environment.time_step_spec().step_type)
 print('time_step_spec.discount:', environment.time_step_spec().discount)
 print('time_step_spec.reward:', environment.time_step_spec().reward)
+
+action = np.array(1, dtype=np.int32)
+time_step = environment.reset()
+print(time_step)
+while not time_step.is_last():
+  time_step = environment.step(action)
+  print(time_step)
